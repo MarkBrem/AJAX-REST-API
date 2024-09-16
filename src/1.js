259334e375ea8df7baa96982170153fae629eead
Марко <mgusanica@gmail.com>
2024-09-16 20:30:31 +0300

    опа

--- src/1.js
+++ src/1.js
@@ -1,20 +1,26 @@
 const formRef = document.querySelector('.js-search-form');
 const articlesContainerRef = document.querySelector('.js-articles-container');
+const loadMoreBtnRef = document.querySelector('[data-action="load-more"]')
+
+formRef.addEventListener('submit', onSearch)
+
+
 
 const BASE_URl = 'https://newsapi.org/v2/everything'
 const API_KEY = 'eb07b8ce736e46c8aa360eafd431418e'
 
+const options = {
+  headers: {
+    Authorization: API_KEY
+  }
+}
+
 function getArticlesByName(query){
-    return fetch(`${BASE_URl}?q=${query}&apiKey=${API_KEY}`)
+    return fetch(`${BASE_URl}?q=${query}&pageSize=20&page=1`, options)
     .then(response=>response.json())
 }
 
-getArticlesByName('car')
-.then(result=>result.articles)
-.then(articles=>{
-    const markup = createArticleCards(articles)
-    articlesContainerRef.innerHTML = markup
-})
+
 
 function createArticleCards(articles){
     return articles.map(article=>{
@@ -31,4 +37,14 @@ function createArticleCards(articles){
   </li>
         `
     })
+}
+
+function onSearch(event){
+  event.preventDefault()
+  getArticlesByName(event.currentTarget.elements.query.value)
+.then(result=>result.articles)
+.then(articles=>{
+    const markup = createArticleCards(articles)
+    articlesContainerRef.innerHTML = markup
+})
 }
