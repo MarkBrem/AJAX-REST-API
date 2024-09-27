b600ca6b9d499f77764107339c05dbc0ac5393b4
Марко <mgusanica@gmail.com>
2024-09-27 19:19:46 +0300

    пвапв

--- src/1.js
+++ src/1.js
@@ -1,50 +1,52 @@
+import { NewsApiService } from "./service-api";
+
 const formRef = document.querySelector('.js-search-form');
 const articlesContainerRef = document.querySelector('.js-articles-container');
-const loadMoreBtnRef = document.querySelector('[data-action="load-more"]')
+const loadMoreBtnRef = document.querySelector('[data-action="load-more"]') 
 
 formRef.addEventListener('submit', onSearch)
+loadMoreBtnRef.addEventListener('click', onLoadMore)
 
+const newsApiService = new NewsApiService()
+console.log(newsApiService);
+function onSearch(e) {
+    e.preventDefault()
+    const form = e.currentTarget;    
+    newsApiService.searchQuery = form.elements.query.value;
 
-
-const BASE_URl = 'https://newsapi.org/v2/everything'
-const API_KEY = 'eb07b8ce736e46c8aa360eafd431418e'
-
-const options = {
-  headers: {
-    Authorization: API_KEY
-  }
+    newsApiService.searchArticles()
+    .then(res => res.articles)
+    .then(articles => {
+        const markup = createArticleCards(articles)
+        articlesContainerRef.insertAdjacentHTML('beforeend', markup) 
+        newsApiService.page += 1    
+    })
+    form.reset()
 }
 
-function getArticlesByName(query){
-    return fetch(`${BASE_URl}?q=${query}&pageSize=20&page=1`, options)
-    .then(response=>response.json())
-}
+function onLoadMore() {
+    newsApiService.searchArticles()
+    .then(res => res.articles)
+    .then(articles => {
+        const markup = createArticleCards(articles)
+        articlesContainerRef.insertAdjacentHTML('beforeend', markup) 
+        newsApiService.page += 1    
+    })
+}  
 
-
-
-function createArticleCards(articles){
-    return articles.map(article=>{
+function createArticleCards(articles) {
+    return articles.map(article => {
         return `
         <li>
-    <a href="${article.url}" target="_blank" rel="noopener noreferrer">
-      <article>
-        <img src="${article.urlToImage}" alt="" width="480">
-        <h2>${article.title}</h2>
-        <p>Posted by:${article.author}</p>
-        <p>${article.description}</p>
-      </article>
-    </a>
-  </li>
+            <a href="${article.url}" target="_blank" rel="noopener noreferrer">
+            <article>
+                <img src="${article.urlToImage}" alt="" width="480">
+                <h2>${article.title}</h2>
+                <p>Posted by: ${article.author}</p>
+                <p>${article.description}</p>
+            </article>
+            </a>
+        </li>
         `
     })
-}
-
-function onSearch(event){
-  event.preventDefault()
-  getArticlesByName(event.currentTarget.elements.query.value)
-.then(result=>result.articles)
-.then(articles=>{
-    const markup = createArticleCards(articles)
-    articlesContainerRef.innerHTML = markup
-})
 }
---
+++ src/service-api.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://newsapi.org/v2/everything/';
+const API_KEY = '3ce63eea477043d7a470d2b21dc5ab4b';
+
+const options = {
+    headers: {
+        'Authorization': API_KEY
+    }
+}
+
+export class NewsApiService {
+    constructor() {
+        this.page = 1;
+        this.searchQuery = '';
+    }
+
+    searchArticles = () => {
+        return fetch(`${BASE_URL}?q=${this.searchQuery}&pageSize=20&page=${this.page}`, options)
+            .then(response => response.json())    
+    }
+
+}
